9115ce6e8e099ceb71f8733ea949a9cc45d27e25
milkyontheblock <jordanvanwijnen0709@gmail.com>
2024-04-20 16:34:06 +0200

    Added remote app key

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: ['joiweterjweoijtoijwetjoi']
+    keys: ['L23U7LrlbC6NjaGYpmrFfrbfFZ4fRYxtM5b/EAlBp9o=']
   },
 });
